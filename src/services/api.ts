934264df76ebf0d77ca06fded49652d6af4efcf6
Isabel <im03moraes@gmail.com>
2023-11-24 20:44:19 +0000

    abrindo branch para desenvolvimento do requisito 4

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId: string, query: string) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe.
   const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   return data.json();
 }
