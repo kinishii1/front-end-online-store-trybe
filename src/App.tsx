00f68ff2fc4098a253557509232eae3afc0d7baa
giovannalavarini <giovanalavarini.code@gmail.com>
2023-11-25 09:57:38 -0300

    correções de lint

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,18 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 
 function App() {
-  const [cartList, setCartList] = useState<any[]>([]);
+  const [cartList] = useState<any[]>([]);
   return (
     <Routes>
       {/* Pagina Inicial - Requisito 2 */}
       <Route
         path="/"
-        element={ <Home /> }
+        element={
+          <Home />
+      }
       />
-      {/* Página de Detalhes do Produto - Novo Requisito */}
-      <Route
-        path="/product/:productId"
-        element={ <ProductDetails /> }
-      />
-      {/* Página de Carrinho - Requisito 3 */}
-      <Route
-        path="/cart"
-        element={ <Cart cartList={ cartList } /> }
-      />
+      {/* Pagina de Carrinho - Requisito 3 */}
+      <Route path="/cart" element={ <Cart cartList={ cartList } /> } />
     </Routes>
   );
 }
